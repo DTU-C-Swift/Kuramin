0b0b7d3d62e95da3fdfd74eef58301131c086e6d
numanbashir <sportybashir@gmail.com>
2023-02-16 10:59:04 +0100

    Working on navigation

--- Kuramin/ContentView.swift
+++ Kuramin/ContentView.swift
@@ -9,50 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
-            Button(action: {
-                
-            }) {
-                HStack {
-                    Image(systemName: "applelogo")
-                        .font(.system(size: 24))
-                        .foregroundColor(.white)
-                    Text("Login with Apple")
-                        .font(.system(size: 24, weight:
-                                .semibold))
-                                .foregroundColor(.white)
+        
+        NavigationView {
+            VStack {
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Image(systemName: "applelogo")
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                        Text("Login with Apple")
+                            .font(.system(size: 24, weight:
+                                    .semibold))
+                                    .foregroundColor(.white)
+                    }
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 24)
+                    .background(Color.black)
+                    .cornerRadius(16)
+                    .shadow(radius: 4, x: 0, y: 4)
+                    
                 }
-                .padding(.vertical, 16)
-                .padding(.horizontal, 24)
-                .background(Color.black)
-                .cornerRadius(16)
-                .shadow(radius: 4, x: 0, y: 4)
+                
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Image(systemName: "book")
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                        Text("Login with Facebook")
+                            .font(.system(size: 24, weight:
+                                    .semibold))
+                                    .foregroundColor(.white)
+                    }
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 24)
+                    .background(Color.blue)
+                    .cornerRadius(16)
+                    .shadow(radius: 4, x: 0, y: 4)
+                    
+                }
                 
             }
-            
-            Button(action: {
-                
-            }) {
-                HStack {
-                    Image(systemName: "book")
-                        .font(.system(size: 24))
-                        .foregroundColor(.white)
-                    Text("Login with Facebook")
-                        .font(.system(size: 24, weight:
-                                .semibold))
-                                .foregroundColor(.white)
-                }
-                .padding(.vertical, 16)
-                .padding(.horizontal, 24)
-                .background(Color.blue)
-                .cornerRadius(16)
-                .shadow(radius: 4, x: 0, y: 4)
-                
-            }
-            
+            .padding()
         }
-        .padding()
+        
+        
     }
 }
 
@@ -60,5 +64,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewInterfaceOrientation(.landscapeRight)
+    }
+}
+
+struct MainPageView: View {
+    var body: some View {
+        Text("This is going to be the main page")
+            .navigationBarTitle("Main Page")
     }
 }
