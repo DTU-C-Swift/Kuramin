41b9b3c977eb56468c5b278f55d34bf4e237b26d
NumanBashir <sportybashir@gmail.com>
2023-02-16 13:20:15 +0100

    added new button and remove back button

--- Kuramin/ContentView.swift
+++ Kuramin/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                 
                 NavigationLink(destination: MainPageView()) {
                     HStack {
-                        Image(systemName: "book")
+                        Image(systemName: "book.fill")
                             .font(.system(size: 24))
                             .foregroundColor(.white)
                         Text("Login with Facebook")
@@ -41,6 +41,23 @@ struct ContentView: View {
                     .padding(.vertical, 16)
                     .padding(.horizontal, 24)
                     .background(Color.blue)
+                    .cornerRadius(16)
+                    .shadow(radius: 4, x: 0, y: 4)
+                }
+                
+                NavigationLink(destination: MainPageView()) {
+                    HStack {
+                        Image(systemName: "tree")
+                            .font(.system(size: 24))
+                            .foregroundColor(.black)
+                        Text("Login with Google")
+                            .font(.system(size: 24, weight:
+                                    .semibold))
+                                    .foregroundColor(.black)
+                    }
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 24)
+                    .background(Color.white)
                     .cornerRadius(16)
                     .shadow(radius: 4, x: 0, y: 4)
                 }
@@ -64,5 +81,6 @@ struct MainPageView: View {
     var body: some View {
         Text("This is going to be the main page")
             .navigationBarTitle("Main Page")
+            .navigationBarBackButtonHidden(true)
     }
 }
