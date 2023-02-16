f84d3db335d1ba39f9ae2cb1ae456dec915a015e
NumanBashir <sportybashir@gmail.com>
2023-02-16 10:13:29 +0100

    Initial Commit

---
+++ Kuramin/KuraminApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuraminApp.swift
+//  Kuramin
+//
+//  Created by Numan Bashir on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct KuraminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
