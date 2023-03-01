0662adab036d03df51c02d8c850c611bf749c4ec
Zahed <zahedm45@gmail.com>
2023-03-01 19:44:13 +0100

    Created a separate view for the game page  background

---
+++ Kuramin/Views/ContecViews/BackgroundView.swift
@@ -0,0 +1,31 @@
+//
+//  BackgroundView.swift
+//  Kuramin
+//
+//  Created by MD. Zahed on 01/03/2023.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient(colors: [Color.black, Color.cyan, Color.blue, Color.black]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: .trailing)
+            .ignoresSafeArea()
+        RoundedRectangle(cornerRadius: 150)
+            .strokeBorder(Color.cyan, lineWidth: 35)
+            .scaleEffect(0.68)
+            .padding(.top, 30)
+        RoundedRectangle(cornerRadius: 150)
+            .strokeBorder(Color.black, lineWidth: 30)
+            .scaleEffect(0.7)
+            .shadow(color: Color.cyan, radius: 10, x: 0, y: 5)
+            .padding(.top, 30)
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}
--- Kuramin/Views/GamePageView.swift
+++ Kuramin/Views/GamePageView.swift
@@ -21,18 +21,7 @@ struct GamePageView: View {
     var body: some View {
         ZStack {
             
-            LinearGradient(gradient: Gradient(colors: [Color.black, Color.cyan, Color.blue, Color.black]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: .trailing)
-                .ignoresSafeArea()
-            RoundedRectangle(cornerRadius: 150)
-                .strokeBorder(Color.cyan, lineWidth: 35)
-                .scaleEffect(0.68)
-                .padding(.top, 30)
-            RoundedRectangle(cornerRadius: 150)
-                .strokeBorder(Color.black, lineWidth: 30)
-                .scaleEffect(0.7)
-                .shadow(color: Color.cyan, radius: 10, x: 0, y: 5)
-                .padding(.top, 30)
-
+            BackgroundView()
             
             VStack {
                 
@@ -63,6 +52,8 @@ struct GamePageView: View {
                 
                 
                 HStack() {
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(.white)
                     Spacer()
                     ZStack {
                         Rectangle()
