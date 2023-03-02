0a6b1bcc6426314bd94dc2050c92c9de25d43923
Zahed <zahedm45@gmail.com>
2023-03-02 13:08:37 +0100

    Reading data from db works

--- Kuramin/KuraminApp.swift
+++ Kuramin/KuraminApp.swift
@@ -10,20 +10,24 @@ import FirebaseCore
 import FirebaseFirestore
 
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
-        return true
-    }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication,
+//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//
+//        return true
+//    }
+//}
 
 @main
 struct KuraminApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
-    
+    init() {
+        
+        FirebaseApp.configure()
+        
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
