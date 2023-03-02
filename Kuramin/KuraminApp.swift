8a53d5a9889a9258eda40f7782971eb8688198ab
Zahed <zahedm45@gmail.com>
2023-03-02 13:09:11 +0100

    Cleaned up

--- Kuramin/KuraminApp.swift
+++ Kuramin/KuraminApp.swift
@@ -9,24 +9,12 @@ import SwiftUI
 import FirebaseCore
 import FirebaseFirestore
 
-
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    func application(_ application: UIApplication,
-//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        FirebaseApp.configure()
-//
-//        return true
-//    }
-//}
-
 @main
 struct KuraminApp: App {
-    //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     init() {
         
         FirebaseApp.configure()
-        
     }
     var body: some Scene {
         WindowGroup {
