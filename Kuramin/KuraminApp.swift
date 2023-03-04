4f5b036fbd6292721bcf748394c2ad19355aa055
Zahed <zahedm45@gmail.com>
2023-03-04 14:46:03 +0100

    Fb button works

--- Kuramin/KuraminApp.swift
+++ Kuramin/KuraminApp.swift
@@ -7,17 +7,21 @@
 
 import SwiftUI
 import Firebase
+import FBSDKCoreKit
 
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        return true
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-}
-
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return ApplicationDelegate.shared.application(app, open: url, options: options)
+    } }
 
 
 @main
