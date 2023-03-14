f3b83902387e5c111cfd3c02da0e7c642a8c12f6
Zahed <zahedm45@gmail.com>
2023-03-14 23:20:50 +0100

    Created a file for Printer class

--- Kuramin/Utility/Util.swift
+++ Kuramin/Utility/Util.swift
@@ -9,30 +9,6 @@ import Foundation
 import SwiftUI
 
 
-class Printer {
-    var tag: String
-    var displayPrints: Bool
-    
-    init(tag: String, displayPrints: Bool) {
-        self.tag = tag
-        self.displayPrints = displayPrints
-    }
-    
-    func write(_ str: Any) {
-        print(tag,": ", str)
-
-    }
-    
-    func writeRed(_ str: Any) {
-        //print(tag,": ", str, modifier: .red )
-        //print("\(tag): \("\u{001B}[31m\(str)\u{001B}[0m")")
-        self.write(str)
-
-
-    }
-    
-    
-}
 
 struct Util {
     let PROGRESSING = "display"
