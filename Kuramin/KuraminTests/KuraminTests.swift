4b891de04141b1fd5cb52f397d8ba3fd32083c83
Zahed <zahedm45@gmail.com>
2023-03-15 20:21:51 +0100

    Test class removed

--- Kuramin/KuraminTests/KuraminTests.swift
+++ Kuramin/KuraminTests/KuraminTests.swift
@@ -15,4 +15,79 @@ final class KuraminTests: XCTestCase {
 
 
 
+    
+  //    func testAddPlayer() throws {
+  //        var controller = Controller()
+  //        let LOBBY = "lobby"
+  //
+  //        var game = controller.game
+  //
+  //
+  //
+  //
+  //
+  //
+  //        var p1 = Player(id: UUID())
+  //        var p2 = Player(id: UUID())
+  //        var p3 = Player(id: UUID())
+  //        var p4 = Player(id: UUID())
+  //        var p5 = Player(id: UUID())
+  //        var p6 = Player(id: UUID())
+  //        var p7 = Player(id: UUID())
+  //
+  //
+  //        game.addPlayer(player: p1)
+  //        game.addPlayer(player: p2)
+  //        game.addPlayer(player: p3)
+  //
+  //        XCTAssertEqual(game.actualPlayerSize, 3)
+  //        XCTAssertEqual(game.players.count, 3)
+  //
+  //
+  //        XCTAssertEqual(true, game.players.contains(where: {$0.id == p1.id}))
+  //        XCTAssertEqual(true, game.players.contains(where: {$0.id == p2.id}))
+  //        XCTAssertEqual(true, game.players.contains(where: {$0.id == p3.id}))
+  //
+  //
+  //
+  //        game.addPlayer(player: p4)
+  //        game.addPlayer(player: p5)
+  //        game.addPlayer(player: p6)
+  //        game.addPlayer(player: p7)
+  //
+  //
+  //        XCTAssertEqual(game.actualPlayerSize, 7)
+  //        XCTAssertEqual(game.players.count, 7)
+  //        // Adding players checked
+  //
+  //
+  //        var p8 = Player(id: UUID())
+  //        game.addPlayer(player: p8)
+  //
+  //        XCTAssertEqual(false, game.players.contains(where: {$0.id == p8.id}))
+  //
+  //
+  //
+  //
+  //
+  //
+  //        var p9 = Player(id: UUID())
+  //        var p10 = Player(id: UUID())
+  //
+  //
+  //        var lobby = Lobby(host: p1.id, [p8, p9, p10])
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //    }
+    
+    
+    
 }
