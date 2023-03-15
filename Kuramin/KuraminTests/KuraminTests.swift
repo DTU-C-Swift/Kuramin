959312ca3be1a14580d5621e1d2c43f428c4fe4c
Zahed <zahedm45@gmail.com>
2023-03-15 20:14:33 +0100

    ....

--- Kuramin/KuraminTests/KuraminTests.swift
+++ Kuramin/KuraminTests/KuraminTests.swift
@@ -4,13 +4,12 @@
 //
 //  Created by MD. Zahed on 15/03/2023.
 //
-
 import XCTest
-import Kuramin
 
 final class KuraminTests: XCTestCase {
-
+    
     func testExample() throws {
+        
         XCTAssertEqual(2, 2)
     }
 
