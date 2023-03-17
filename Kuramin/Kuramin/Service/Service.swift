91664418d8fdb1ec3ad9594926277ba0b6d46ce8
Zahed <zahedm45@gmail.com>
2023-03-17 18:24:37 +0100

    Cleaned up Service class

--- Kuramin/Kuramin/Service/Service.swift
+++ Kuramin/Kuramin/Service/Service.swift
@@ -33,185 +33,6 @@ class Service {
     
     
     
-    func create_or_update_user(userImage: UIImage?) {
-        let game = DataHolder.controller.game
-
-        if userImage == nil {
-            self.printer.write("Image is nil")
-            self.logOut()
-            return
-            
-        }
-        // TODO check if the user is already exist in the db
-                
-        
-        if let user = Auth.auth().currentUser {
-            var dbUser = DbUser(uid: user.uid, fullName: user.displayName ?? "", coins: 500)
-            
-            game.me.setId(pid: user.uid)
-            
-            
-            let userRef = db.collection("users").document(user.uid)
-            
-            userRef.getDocument { document, err in
-                
-                if let document = document, document.exists {
-                    if let coins = document.data()?["coins"] as? Int {
-                        self.printer.write("User already exits in the db")
-                        dbUser.coins = coins
-                        
-                    }
-                    
-                }
-                
-                self.createUser(dbUser, userImage!)
-                
-                
-            }
-        }
-        
-    }
-    
-    
-    
-    
-    func createUser(_ dbUser: DbUser, _ userImage: UIImage) {
-        
-        if let uid = dbUser.uid {
-            
-            do {
-                try db.collection("users").document(uid).setData(from: dbUser)
-
-            } catch let error {
-                self.printer.write("Error creating user in db: \(error)")
-            }
-            
-            uploadImg(userId: uid, img: userImage)
-            
-        } else {
-            self.printer.write("User in nil")
-            self.logOut()
-        }
-        
-        
-    }
-    
-    
-    
-    
-    
-    
-    private func uploadImg(userId: String, img: UIImage) {
-        
-        guard let imageData = img.jpegData(compressionQuality: 0.8) else {
-            //self.logOut()
-            self.printer.write("Error converting image")
-            return
-        }
-        
-        let path = storage.reference().child("images").child(userId)
-        let filename = "\(userId).jpg"
-        let imageRef = path.child(filename)
-        
-        
-        let metadata = StorageMetadata()
-        metadata.contentType = "image/jpeg"
-        
-        let uploadTask = imageRef.putData(imageData, metadata: metadata) { metadata, error in
-            if let error = error {
-                self.printer.write("Error uploading image: \(error.localizedDescription)")
-                return
-            }
-            
-            self.printer.write("Image uploaded successfully!")
-        }
-        
-    }
-    
-    
-    
-    
-    
-    func observeMeInDB() {
-        let game = DataHolder.controller.game
-        let me = game.me
-        
-        if me.id == Util().MY_DUMMY_ID {
-            
-            if let user = Auth.auth().currentUser {
-                me.setId(pid: user.uid)
-            }
-        }
-        
-        
-
-        
-        // Gets user image from storage
-        self.downloadImg(player: me)
-        
-        
-        
-        // Gets user from firestore
-        db.collection("users").document(me.id).addSnapshotListener { snapshot, error in
-            guard let document = snapshot else {
-                self.printer.write("Error fetching document: \(error!)")
-                
-                return
-            }
-            guard let data = document.data() else {
-                self.printer.write("Document data was empty.")
-                return
-            }
-            
-            do {
-                let dbUser = try document.data(as: DbUser.self)
-                self.printer.write("Retrieved user: \(dbUser.toString())")
-                me.updateMe(dbUser: dbUser)
-                
-            }
-            catch{
-                self.printer.write("getUser failed: \(data)")
-                
-            }
-            
-            
-        }
-        
-        
-    }
-    
-    
-    
-    // Downloads the profile picture of the given player and sets the fetched picture to the given player.
-    
-    func downloadImg(player: Player) {
-        
-        let path = storage.reference().child("images").child(player.id)
-        let filename = "\(player.id).jpg"
-        let imageRef = path.child(filename)
-        imageRef.getData(maxSize: 1 * 100 * 100) { data, error in
-            if let error = error {
-                self.printer.write("Error occured while fetching imge for UID: \(player.id), error: \(error)")
-                
-            }
-            else {
-                if let img = UIImage(data: data!) {
-                    
-                    player.setStrImg(img: img)
-                                        
-                    //DataHolder.playerGerbage.append(player)
-
-                    return
-                }
-                self.printer.write("Error in converting image")
-
-            }
-        }
-        
-    }
-    
-    
-    
     
     
     
@@ -417,53 +238,224 @@ class Service {
     
     
     
+    // ----------------------------------------------- //
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func create_or_update_user(userImage: UIImage?) {
+        let game = DataHolder.controller.game
+
+        if userImage == nil {
+            self.printer.write("Image is nil")
+            self.logOut()
+            return
+            
+        }
+        // TODO check if the user is already exist in the db
+                
+        
+        if let user = Auth.auth().currentUser {
+            var dbUser = DbUser(uid: user.uid, fullName: user.displayName ?? "", coins: 500)
+            
+            game.me.setId(pid: user.uid)
+            
+            
+            let userRef = db.collection("users").document(user.uid)
+            
+            userRef.getDocument { document, err in
+                
+                if let document = document, document.exists {
+                    if let coins = document.data()?["coins"] as? Int {
+                        self.printer.write("User already exits in the db")
+                        dbUser.coins = coins
+                        
+                    }
+                    
+                }
+                
+                self.createUser(dbUser, userImage!)
+                
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    func createUser(_ dbUser: DbUser, _ userImage: UIImage) {
+        
+        if let uid = dbUser.uid {
+            
+            do {
+                try db.collection("users").document(uid).setData(from: dbUser)
+
+            } catch let error {
+                self.printer.write("Error creating user in db: \(error)")
+            }
+            
+            uploadImg(userId: uid, img: userImage)
+            
+        } else {
+            self.printer.write("User in nil")
+            self.logOut()
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    private func uploadImg(userId: String, img: UIImage) {
+        
+        guard let imageData = img.jpegData(compressionQuality: 0.8) else {
+            //self.logOut()
+            self.printer.write("Error converting image")
+            return
+        }
+        
+        let path = storage.reference().child("images").child(userId)
+        let filename = "\(userId).jpg"
+        let imageRef = path.child(filename)
+        
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        let uploadTask = imageRef.putData(imageData, metadata: metadata) { metadata, error in
+            if let error = error {
+                self.printer.write("Error uploading image: \(error.localizedDescription)")
+                return
+            }
+            
+            self.printer.write("Image uploaded successfully!")
+        }
+        
+    }
+    
+    
+    
+    
+    
+    func observeMeInDB() {
+        let game = DataHolder.controller.game
+        let me = game.me
+        
+        if me.id == Util().MY_DUMMY_ID {
+            
+            if let user = Auth.auth().currentUser {
+                me.setId(pid: user.uid)
+            }
+        }
+        
+        
+
+        
+        // Gets user image from storage
+        self.downloadImg(player: me)
+        
+        
+        
+        // Gets user from firestore
+        db.collection("users").document(me.id).addSnapshotListener { snapshot, error in
+            guard let document = snapshot else {
+                self.printer.write("Error fetching document: \(error!)")
+                
+                return
+            }
+            guard let data = document.data() else {
+                self.printer.write("Document data was empty.")
+                return
+            }
+            
+            do {
+                let dbUser = try document.data(as: DbUser.self)
+                self.printer.write("Retrieved user: \(dbUser.toString())")
+                me.updateMe(dbUser: dbUser)
+                
+            }
+            catch{
+                self.printer.write("getUser failed: \(data)")
+                
+            }
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
+    // Downloads the profile picture of the given player and sets the fetched picture to the given player.
+    
+    func downloadImg(player: Player) {
+        
+        let path = storage.reference().child("images").child(player.id)
+        let filename = "\(player.id).jpg"
+        let imageRef = path.child(filename)
+        imageRef.getData(maxSize: 1 * 100 * 100) { data, error in
+            if let error = error {
+                self.printer.write("Error occured while fetching imge for UID: \(player.id), error: \(error)")
+                
+            }
+            else {
+                if let img = UIImage(data: data!) {
+                    
+                    player.setStrImg(img: img)
+                                        
+                    //DataHolder.playerGerbage.append(player)
+
+                    return
+                }
+                self.printer.write("Error in converting image")
+
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     //------------------------------------------ Old implementation of lobby ---------------------------------//
     
-//    func goToLobby() {
-//        let game = DataHolder.controller.game
-//        let me = game.me
-//        let ref = db.collection("matchMaker").document(lobbyStr)
-//        //player.id = "testId"
-//
-//        // Transactional call
-//        db.runTransaction({ (transaction, errorPointer) -> Any? in
-//            let lobbyDocument: DocumentSnapshot
-//            do {
-//                lobbyDocument = try transaction.getDocument(ref)
-//            } catch let fetchError as NSError {
-//                errorPointer?.pointee = fetchError
-//                return nil
-//            }
-//
-//
-//            // Checks if document "lobby" exists already in db
-//            if !lobbyDocument.exists {
-//                transaction.setData(["playerIds": [me.id], "host": me.id], forDocument: ref)
-//            } else {
-//                transaction.updateData([
-//                    "playerIds" : FieldValue.arrayUnion([me.id])
-//                ], forDocument: ref)
-//            }
-//
-//            return nil
-//
-//        }) { (object, error) in
-//            if let error = error {
-//                self.printer.write("Transaction failed: \(error)")
-//            } else {
-//                self.printer.write("Transaction succeeded!")
-//                self.amIHost(game: game)
-//                self.observeLobby(game: game)
-//            }
-//        }
-//    }
-//
-//
-//
-//
-//
+
+
 //    func amIHost(game: Game) {
 //        let me = game.me
 //
