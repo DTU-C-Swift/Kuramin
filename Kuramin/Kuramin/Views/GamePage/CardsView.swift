03bd757d5067b85152ec5b26a4467c16a6f6054e
Zahed <zahedm45@gmail.com>
2023-03-26 13:26:51 +0200

    Working on click on cards in game page

--- Kuramin/Kuramin/Views/GamePage/CardsView.swift
+++ Kuramin/Kuramin/Views/GamePage/CardsView.swift
@@ -14,15 +14,15 @@ struct CardsView: View {
     
     init(me: Player) {
         self.me = me
-//        me.addCard(card: Card(suit: "H", value: 10))
-//        me.addCard(card: Card(suit: "D", value: 10))
-//        me.addCard(card: Card(suit: "S", value: 10))
-//        me.addCard(card: Card(suit: "C", value: 10))
-//
-//        me.addCard(card: Card(suit: "H", value: 10))
-//        me.addCard(card: Card(suit: "D", value: 10))
-//        me.addCard(card: Card(suit: "S", value: 10))
-//        me.addCard(card: Card(suit: "C", value: 10))
+        //        me.addCard(card: Card(suit: "H", value: 10))
+        //        me.addCard(card: Card(suit: "D", value: 10))
+        //        me.addCard(card: Card(suit: "S", value: 10))
+        //        me.addCard(card: Card(suit: "C", value: 10))
+        //
+        //        me.addCard(card: Card(suit: "H", value: 10))
+        //        me.addCard(card: Card(suit: "D", value: 10))
+        //        me.addCard(card: Card(suit: "S", value: 10))
+        //        me.addCard(card: Card(suit: "C", value: 10))
         
         
     }
@@ -34,39 +34,86 @@ struct CardsView: View {
             
             
             if me.cards.count > 0 {
-                CardView(card: me.cards[0])
+                
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[0])
+                    
+                }
             }
+            
             
             if me.cards.count > 1 {
-                CardView(card: me.cards[1])
+                
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[1])
+                }
             }
+            
             
             if me.cards.count > 2 {
-                CardView(card: me.cards[2])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[2])
+                }
             }
+            
             
             if me.cards.count > 3 {
-                CardView(card: me.cards[3])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[3])
+                }
             }
+            
+            
             
             if me.cards.count > 4 {
-                CardView(card: me.cards[4])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[4])
+                }
             }
             
+            
             if me.cards.count > 5 {
-                CardView(card: me.cards[5])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[5])
+                }
             }
             
             if me.cards.count > 6 {
-                CardView(card: me.cards[6])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[6])
+                }
             }
-    
+            
+            
             if me.cards.count > 7 {
-                CardView(card: me.cards[7])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[7])
+                }
             }
-    
+            
+            
             if me.cards.count > 8 {
-                CardView(card: me.cards[8])
+                Button(action: {
+                    
+                }) {
+                    CardView(card: me.cards[8])
+                }
             }
             
         }
